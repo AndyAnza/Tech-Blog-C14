a42ffc2cde778cf5ba6f13ec95382d28cb8acbca
Andrea Ramirez <andrea.anza3@gmail.com>
2023-03-18 00:04:15 -0600

    Front End - added new post button, edit-delete form, finished login and sign up form

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,1 +1,8 @@
-
+//home - all existing posts from all users
+//home - comment  TODO
+//dashboard - with existing user posts TODO
+//dashboard - new post button to form
+//newpost - new post form
+//editpost - form where user can edit or delete the selected post
+//login - login form
+//signin - signin form
