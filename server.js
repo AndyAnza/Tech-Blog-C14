7083eb6f763c74acd3ac2fd3b96101c0f832fa30
Andrea Ramirez <andrea.anza3@gmail.com>
2023-03-30 22:04:08 -0600

    Added index and homeRouts - main.handlebars file is not being rendered

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const sequelize = require('./config/connection');
 const express = require('express');
-const exphbs = require('express-handlebars');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const routes = require('./controllers');
+const hbs = handlebars.create({
+  // Specify the folder for partials
+  partialsDir: 'views/partials',
+  // Specify the layout template
+  defaultLayout: 'views/layouts',
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const { User, Post, Comment } = require('./model');
+app.use(routes);
+
+// app.get('/', function (req, res) {
+//   res.render('main');
+// });
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening on Port 3000'));
 });
